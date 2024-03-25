2bec470b90b369ce625a1d6f3a086e5006230301
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-03-25 18:44:31 +0100

    Update: implement the new JSON writer in the healthcheck handler

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,13 +1,22 @@
 package main
 
 import (
-  "fmt"
   "net/http"
 )
 
 
 func (app *application) healthcheckHandler(res http.ResponseWriter, req *http.Request) {
-  fmt.Fprintln(res, "status: available")
-  fmt.Fprintf(res, "environment: %s\n", app.config.env)
-  fmt.Fprintf(res, "version: %s\n", version)
+  env := envelope{
+    "status": "available",
+    "system_info": map[string]string{
+      "environment": app.config.env,
+      "version": version,
+    },
+  }
+
+  err := app.writeJSON(res, http.StatusOK, env, nil)
+  if err != nil {
+    app.logger.Println(err)
+    http.Error(res, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+  }
 }
