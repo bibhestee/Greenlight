dbbaf02bc12dc745d388736909ef31c466899cfb
Bibest <bibhestee@gmail.com>
2024-06-02 17:30:40 +0100

    improvement: implemented a graceful shutdown with goroutine to manage unfinished request

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-  "context"
-  "database/sql"
-  "flag"
-  "fmt"
-  "log"
-  "net/http"
-  "os"
-  "time"
-  "github.com/bibhestee/Greenlight/internal/data"
-  "github.com/bibhestee/Greenlight/internal/jsonlog"
-  _ "github.com/lib/pq"
+	"context"
+	"database/sql"
+	"flag"
+	"github.com/bibhestee/Greenlight/internal/data"
+	"github.com/bibhestee/Greenlight/internal/jsonlog"
+	_ "github.com/lib/pq"
+	"os"
+	"time"
 )
 
 // App version
@@ -19,91 +16,80 @@ const version = "1.0.0"
 
 // Application configuration
 type config struct {
-  port int
-  env string
-  db  struct {
-    dsn string
-    maxOpenConns int
-    maxIdleConns int
-    maxIdleTime string
-  }
-  limiter struct {
-    rps float64
-    burst int
-    enabled bool
-  }
+	port int
+	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
+	limiter struct {
+		rps     float64
+		burst   int
+		enabled bool
+	}
 }
 
 // Application dependency injector
 type application struct {
-  config config
-  logger *jsonlog.Logger
-  models data.Models
+	config config
+	logger *jsonlog.Logger
+	models data.Models
 }
 
 func main() {
-  var cfg config
+	var cfg config
 
-  flag.IntVar(&cfg.port, "port", 4000, "API server port")
-  flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
-  flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
-  flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
-  flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
-  flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
-  flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
-  flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
-  flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 
-  flag.Parse()
+	flag.Parse()
 
-  logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 
-  db, err := openDB(cfg)
-  if err != nil {
-    logger.PrintFatal(err, nil)
-  }
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.PrintFatal(err, nil)
+	}
 
-  defer db.Close()
+	defer db.Close()
 
-  logger.PrintInfo("database connection pool established", nil)
+	logger.PrintInfo("database connection pool established", nil)
 
-  app := &application{
-    config: cfg,
-    logger: logger,
-    models: data.NewModels(db),
-  }
+	app := &application{
+		config: cfg,
+		logger: logger,
+		models: data.NewModels(db),
+	}
 
-  srv := &http.Server{
-    Addr: fmt.Sprintf(":%d", cfg.port),
-    Handler: app.routes(),
-    ErrorLog: log.New(logger, "", 0),
-    IdleTimeout: time.Minute,
-    ReadTimeout: 10 * time.Second,
-    WriteTimeout: 30 * time.Second,
-  }
+	err = app.serve()
+	if err != nil {
+		logger.PrintFatal(err, nil)
 
-  logger.PrintInfo("starting server", map[string]string{
-    "addr": srv.Addr,
-    "env": cfg.env,
-    })
-  err = srv.ListenAndServe()
-  logger.PrintFatal(err, nil)
+	}
 }
 
-
 func openDB(cfg config) (*sql.DB, error) {
-  db, err := sql.Open("postgres", cfg.db.dsn)
-  if err != nil {
-    return nil, err
-  }
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
 
-  ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-  defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-  err = db.PingContext(ctx)
-  if err != nil {
-    return nil, err
-  }
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
 
-  return db, nil
+	return db, nil
 }
