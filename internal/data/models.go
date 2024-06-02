d7ee1c8ea29e7a16c7c770e17bbd56fd32cfcc95
Bibest <bibhestee@gmail.com>
2024-06-02 20:30:32 +0100

    feat(users): Setup user model and schema: CRUD methods implemented

--- internal/data/models.go
+++ internal/data/models.go
@@ -1,23 +1,23 @@
 package data
 
 import (
-  "database/sql"
-  "errors"
+	"database/sql"
+	"errors"
 )
 
 var (
-  ErrRecordNotFound = errors.New("record not found")
-  ErrEditConflict = errors.New("edit conflict")
+	ErrRecordNotFound = errors.New("record not found")
+	ErrEditConflict   = errors.New("edit conflict")
 )
 
-
 type Models struct {
-  Movies MovieModel
+	Movies MovieModel
+	Users  UserModel
 }
 
-
 func NewModels(db *sql.DB) Models {
-  return Models{
-    Movies: MovieModel{DB: db},
-  }
+	return Models{
+		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db},
+	}
 }
