aa0db8d218c08688f3bc7fb80988169623874300
Bibest <bibhestee@gmail.com>
2024-06-09 16:44:57 +0100

    fix(typo): replace general feedback with duplicate email response on multiple email conflict

--- internal/data/users.go
+++ internal/data/users.go
@@ -97,7 +97,7 @@ func (m UserModel) Insert(user *User) error {
 
 	if err != nil {
 		switch {
-		case err.Error() == `pq: duplicate key value violates unique constraint "user_email_key"`:
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
 			return ErrDuplicateEmail
 		default:
 			return err
